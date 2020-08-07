64a75c1a9f6b2b1f07277f76f672bb23c3261c17
minwook <tennis0385@naver.com>
2020-08-08 02:32:03 +0900

    추가업데이트

--- assets/js/common.js
+++ assets/js/common.js
@@ -108,3 +108,26 @@ $(function(){
 		setPreview(opt);
 	});
 });
+
+
+
+// 자주 묻는 질문 아코디언 메뉴 활성화
+function accordion ()
+{
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+
+	for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function() {
+		
+		this.classList.toggle("active");
+		var panel = this.nextElementSibling;
+		
+		if (panel.style.display === "block") {
+		panel.style.display = "none";
+		} else {
+		panel.style.display = "block";
+		}
+	});
+	}
+}
